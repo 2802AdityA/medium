67e067b0c2b2d9be7e6d11a75d9e6fcf8f3f3ad8
Aditya Singla <adityasingla.2802@gmail.com>
2024-07-12 23:06:38 +0530

    Add Blog-bulk and single pages

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,4 +1,17 @@
+import { useParams } from "react-router-dom"
+import { useBlog } from "../hooks"
+import { Author, Content } from "../components";
+
 export const Blog = () => {
-    return <div>blog</div>
+    const { id } = useParams()
+    const { loading, blog } = useBlog({ id: id || "" });
+    if (loading) {
+        return <div>loading</div>
+    }
+    return <div className="grid md:grid-cols-12 p-10 m-6 gap-8">
+        <Content title={blog?.title || ""} content={blog?.content || ""} />
+        <Author name={blog?.author.name || ""} />
+        {/* <div>{JSON.stringify(blog)}</div> */}
+    </div>
 
 }
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -14,7 +14,7 @@ export const Blogs = () => {
         <AppBar />
         <div>
             {
-                blogs.map(blog => <BlogCard authorName={"aditya"} title={"Multi-Line Truncation with Tailwind CSS Multi-Line Truncation with Tailwind CSS"} content={`Hi there, in this article I’ll show you guys how to truncate a text using an official Tailwind CSS plugin named line-clamp. Let jump into it. Let assume we have 3 cards which look like this. As you can see, their text length is not the same which is generally not what we want. So, in order to solve this, we could use the default truncate utility in Tailwind CSS. Let have a look how our card gonna looks like if we use this default property.`} publishedDate={"5th June 2020"} />
+                blogs.map(blog => <BlogCard id={blog.id} authorName={blog.author.name || "Anonymous"} title={blog.title} content={blog.content} publishedDate={"5th June 2020"} />
                 )
             }
 
