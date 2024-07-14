885c811f70b540d94143c6d019f75fd7e86076d5
Aditya Singla <adityasingla.2802@gmail.com>
2024-07-15 00:32:50 +0530

    add skeleton loading page to all blogs page

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,4 +1,4 @@
-import { AppBar, BlogCard } from "../components"
+import { AppBar, BlogCard, BlogsSkeleton } from "../components"
 import { useBlogs } from "../hooks"
 
 
@@ -7,11 +7,21 @@ export const Blogs = () => {
     const { loading, blogs } = useBlogs();
 
     if (loading) {
-        return <div>loading..</div>
+        return <div>
+            <AppBar />
+            <BlogsSkeleton />
+            <BlogsSkeleton />
+            <BlogsSkeleton />
+            <BlogsSkeleton />
+            <BlogsSkeleton />
+            <BlogsSkeleton />
+
+        </div>
     }
 
     return <div>
         <AppBar />
+
         <div>
             {
                 blogs.map(blog => <BlogCard id={blog.id} authorName={blog.author.name || "Anonymous"} title={blog.title} content={blog.content} publishedDate={"5th June 2020"} />
