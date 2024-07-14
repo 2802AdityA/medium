6ba1d4e5a9f6a1043bd1a6d14c854c993eb87ad7
Aditya Singla <adityasingla.2802@gmail.com>
2024-07-15 00:32:33 +0530

    add skeleton loading page to blog page

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom"
 import { useBlog } from "../hooks"
-import { AppBar, Author, Content } from "../components";
+import { AppBar, Author, BlogSkeleton, Content } from "../components";
 
 export const Blog = () => {
     const { id } = useParams()
     const { loading, blog } = useBlog({ id: id || "" });
     if (loading) {
-        return <div>loading</div>
+        return <div>
+            <AppBar />
+            <BlogSkeleton />
+        </div>
     }
     return <div>
         <AppBar />
